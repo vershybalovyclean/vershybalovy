f5993b65e0bfc60b24aa97ec120a26866e76e83b
vershybalovyclean <vershybalovy@gmail.com>
2026-05-17 20:35:57 +0200

    Delete api/test.js

--- api/test.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  console.log("Test function called!");
-  return res.status(200).json({ 
-    success: true, 
-    time: new Date().toISOString(),
-    message: "API is working!"
-  });
-}
